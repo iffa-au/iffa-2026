319508ed38472f5d4dbacc839358b72659a0c096
Sunil Sharma <sunilpoudel1999@gmail.com>
2026-09-16 13:56:17 +1000

    :sparkles:feat(google-analytics) - Google Search Console Meta Tag added

--- src/modules/shared/components/google-analytics.tsx
+++ src/modules/shared/components/google-analytics.tsx
@@ -1,11 +1,27 @@
-import { GoogleAnalytics } from "@next/third-parties/google";
+import type { Metadata } from "next";
+import { GoogleAnalyticsProvider } from "@/components/GoogleAnalyticsProvider";
 
-type GoogleAnalyticsProps = {
-  gaId: string;
+export const metadata: Metadata = {
+  title: "IFFA Awards",
+  verification: {
+    google: "2f0R1wXJpnxzjUqfrLhTtZ27uJ_NOmUIEV4SPW-TlDA",
+  },
 };
 
-export function GoogleAnalyticsProvider({
-  gaId,
-}: GoogleAnalyticsProps) {
-  return <GoogleAnalytics gaId={gaId} />;
-}   
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+
+        <GoogleAnalyticsProvider
+          gaId={process.env.NEXT_PUBLIC_GA_ID!}
+        />
+      </body>
+    </html>
+  );
+}
